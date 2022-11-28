526ec10afe2d390978512d1ec2bcdd30ae08dc1f
noctrox389 <114263655+noctrox389@users.noreply.github.com>
2022-11-28 08:49:54 -0400

    Add files via upload

---
+++ assets/preload/data/abduction/hideHUD.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+  setProperty('healthBar.visible', false)
+  setProperty('healthBarBG.visible', false)
+  setProperty('iconP1.visible', false)
+  setProperty('iconP2.visible', false)
+  setProperty('iconP2.alpha', 0)
+  setProperty('scoreTxt.visible', false)
+end
